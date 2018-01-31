48f2e4798e1671cc907c9c233bbc16425aad6707
aleebek <a.kazhibekov@mail.ru>
2018-01-31 18:57:10 +0600

    part 5.5 1

--- resources/views/Pages/welcome.blade.php
+++ resources/views/Pages/welcome.blade.php
@@ -1,95 +1,115 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Laravel</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <title>Laravel blog</title>
+  </head>
+  <body>
+  	<!-- Default Bootstrap Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="#">Laravel blog</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+		  <li class="nav-item active">
+			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="/about">About</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="/contact">Contact</a>
+		  </li>
+		  
+		  
+		</ul>	
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item dropdown navbar-right">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				  My Account
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				  <a class="dropdown-item" href="#">Action</a>
+				  <a class="dropdown-item" href="#">Another action</a>
+				  <a class="dropdown-item" href="#">Something else here</a>
+				</div>
+		  	</li>
+		</ul>
+	  </div>
+	  
+	</nav>   
+ 	
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-md-12">
+  				<div class="jumbotron">
+					<h1 class="display-4">Welcome to my blog!</h1>
+					<p class="lead">Thank you so much for visiting my blog</p>
+					<hr class="my-4">
+					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+					<p class="lead">
+						<a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a>
+					</p>
+				</div>
+  			</div>
+  		</div> <!-- End of .row -->
+  		<div class="row">
+  			<div class="col-md-8">
+				<div class="post">
+					<h3>Post Title</h3>
+					<p class="lead">Nam malis elit se proident si sed esse iudicem despicationes. Fore mandaremus id 
+						arbitror ad cernantur nulla doctrina aliquip. Dolor pariatur ad arbitror. Te 
+						cupidatat a vidisse qui quo ...</p>
+					<a href="#" class="btn btn-primary">Read More</a>
+				</div>
+				<hr>
+				<div class="post">
+					<h3>Post Title</h3>
+					<p class="lead">Nam malis elit se proident si sed esse iudicem despicationes. Fore mandaremus id 
+						arbitror ad cernantur nulla doctrina aliquip. Dolor pariatur ad arbitror. Te 
+						cupidatat a vidisse qui quo ...</p>
+					<a href="#" class="btn btn-primary">Read More</a>
+				</div>
+				<hr>
+				<div class="post">
+					<h3>Post Title</h3>
+					<p class="lead">Nam malis elit se proident si sed esse iudicem despicationes. Fore mandaremus id 
+						arbitror ad cernantur nulla doctrina aliquip. Dolor pariatur ad arbitror. Te 
+						cupidatat a vidisse qui quo ...</p>
+					<a href="#" class="btn btn-primary">Read More</a>
+				</div>
+				<hr>
+				<div class="post">
+					<h3>Post Title</h3>
+					<p class="lead">Nam malis elit se proident si sed esse iudicem despicationes. Fore mandaremus id 
+						arbitror ad cernantur nulla doctrina aliquip. Dolor pariatur ad arbitror. Te 
+						cupidatat a vidisse qui quo ...</p>
+					<a href="#" class="btn btn-primary">Read More</a>
+				</div>
+				<hr>
+				
+  			</div>
+  			<div class="col-md-3 col-md-offset-1">
+				<h2>Sidebar</h2>
+  			</div>
+  		</div>
+  	</div> <!-- End of .container -->
+   
+   
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    WELCOME
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
 </html>
