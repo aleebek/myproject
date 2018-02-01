0156e401e9e740b32461ec3ee2dee9b2461f18b1
aleebek <a.kazhibekov@mail.ru>
2018-02-01 18:26:15 +0600

    part 11

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', 'PagesController@getIndex');
 Route::get('/about', 'PagesController@getAbout');
 
 Route::get('/contact', 'PagesController@getContact');
+
+Route::resource('posts', 'PostController');
